8b761b74e9fb44d97d3a4544380536d51693e0f5
ylsky123 <245977646@qq.com>
2017-11-01 21:18:11 +0800

    addswiper

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import App from './app/App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import FastClick from 'fastclick';
+
 FastClick.attach(document.body);
 
 Vue.config.productionTip = false
-
+Vue.use(VueAwesomeSwiper)
 new Vue({
-          el: '#app',
-          router,
-          template: '<App/>',
-          components: { App }
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: {App}
 })
--- src/pages/home/banner.vue
+++ src/pages/home/banner.vue
@@ -1,21 +1,77 @@
+
 <template>
-	<div class="banner">
-		{{msg}}
+	<div class="swiper-box" ref="swiperBox">
+		<swiper :options="swiperOption"  ref="mySwiper">
+			<swiper-slide v-for="item in items" :key="item.id" >
+				<img :src="item.url" class="swiper-image">
+			</swiper-slide>
+			<div class="swiper-pagination" slot="pagination"></div>
+		</swiper>
 	</div>
 </template>
 
 <script>
-
+	import { swiper, swiperSlide } from 'vue-awesome-swiper'
 	export default {
-		data:function () {
+		data() {
 			return {
-				msg: "banner123"
+				items:[{
+					id:1,
+					url:"../../../static/images/1.jpg"
+				},{
+					id:2,
+					url:"../../../static/images/2.jpg"
+				},{
+					id:3,
+					url:"../../../static/images/3.jpg"
+				},{
+					id:4,
+					url:"../../../static/images/4.png"
+				},{
+					id:5,
+					url:"../../../static/images/5.jpg"
+				},{
+					id:6,
+					url:"../../../static/images/6.jpg"
+				},{
+					id:7,
+					url:"../../../static/images/7.jpg"
+				},{
+					id:8,
+					url:"../../../static/images/8.jpg"
+				}],
+				swiperOption: {
+					notNextTick: true,
+					autoplay:3000,
+					loop:true,
+					autoplayDisableOnInteraction:true,
+					direction:'horizontal',
+					paginationTypes:"bullets",
+					pagination: '.swiper-pagination',
+					observeParents:true
+				}
+			}
+		},
+		components: {
+			swiper,
+			swiperSlide
+		},
+
+		computed: {
+
+			swiper() {
+				return this.$refs.mySwiper.swiper;
 			}
 		}
+
 	}
+
+
 </script>
 
-<style>
-
-
+<style scoped>
+	@import "../../../node_modules/swiper/dist/css/swiper.min.css";
+	.swiper-box .swiper-image{
+		width:100%;
+	}
 </style>
